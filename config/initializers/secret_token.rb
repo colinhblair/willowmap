8eef18af4140fa02ba83198648c8b47aaf348352
Colin Blair <colinhblair@Colins-MacBook-Pro.local>
2013-04-14 23:37:08 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Willowmap::Application.config.secret_token = '24fe3b8fdb4b4aa62d81f767cce6abc0c57db1ccd8937adbffe37dc28569229650630f21238c51085c72d008582c066bc59736522a3bf213ceb315d06d4eb766'
